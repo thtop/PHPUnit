93e993fbddda63fdd6b4ff9b1a89a35df755a7cd
Thamonwan Maneechan <thamonwan.top@gmail.com>
2020-02-05 17:12:01 +0700

    finished item 4

---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+// class SampleTest extends \PHPUnit_Framework_TestCase {
+
+class SampleTest extends  \PHPUnit\Framework\TestCase {
+
+  public function testTrueAssertsToTrue() {
+    $this->assertTrue(true);
+  }
+}
